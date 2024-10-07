442e4d7067ffb12e6b4a42a935c040480d378a03
Bishnu K Sahu <sahu.bishnukumari@gmail.com>
2024-10-07 17:42:45 +0000

    commit

--- setup.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ~/.nltk_data
-python -m nltk.downloader -d ~/.nltk_data punkt
-python -m nltk.downloader -d ~/.nltk_data punkt_tab
-python -m nltk.downloader -d ~/.nltk_data stopwords
-python -m nltk.downloader -d ~/.nltk_data vader_lexicon
