7babbfa81bb3f18e702d3b8615a1e5f046a8f0ec
Bishnu K Sahu <sahu.bishnukumari@gmail.com>
2024-10-07 17:06:13 +0000

    push

--- EmailDetectionApp.py
+++ EmailDetectionApp.py
@@ -11,7 +11,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
 nltk.data.path.append('/home/appuser/.nltk_data')
-
+nltk.download('stopwords', download_dir='/home/appuser/.nltk_data')
 
 # Title of the app
 st.title("📧 Email Classification: Spam or Ham")
